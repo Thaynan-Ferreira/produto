572dca8a6ef360645647b202af6760bf4806668f
Thaynan Ferreira <thaynan_ferreira@outlook.com.br>
2022-04-01 20:09:24 -0300

    Continuaçao da aula

---
+++ src/main/java/com/mycompany/produto/ProdutoTeste.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.produto;
+
+/**
+ *
+ * @author univicosa
+ */
+public class ProdutoTeste {
+    
+    public static void main(String[] args) {
+        
+        Produto p1 = new Produto("Roda", "Honda");
+        
+    }
+    
+}
